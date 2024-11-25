0c86e8a7221b11303584595c08068cca401cd0b6
GodsFavour <ifidongodsfavour9@gmail.com>
2024-11-25 16:54:28 +0100

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "576px",
+      },
+    },
   },
   plugins: [],
   base: "/House",
